4b76138f0fd5e30d85c752fcc288a4d1dd573100
Albatrossiun <459627902@qq.com>
2020-08-26 17:02:24 +0800

    initiall

---
+++ test_8_25/calc.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//¶¨Òåº¯Êý
+int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
---
+++ test_8_25/calc.h
@@ -0,0 +1,12 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#ifndef __CALC_H__
+#define __CALC_H__
+
+
+//ÉùÃ÷º¯Êý
+int Add(int x, int y);
+int Sub(int x, int y);
+
+#endif
