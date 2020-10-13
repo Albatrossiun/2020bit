ff420b2db985b77a0078601dc4188ee32c3ab050
Albatrossiun <459627902@qq.com>
2020-10-13 13:59:01 +0800

    initially

--- Project3/源.c
+++ Project3/源.c
@@ -1,40 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//A选手说：B第二，我第三；
-//B选手说：我第二，E第四；
-//C选手说：我第一，D第二；
-//D选手说：C最后，我第三；
-//E选手说：我第四，A第一；
-//比赛结束后，每位选手都说对了一半，请编程确定比赛的名次。
+#include <string.h>
+//实现一个函数，可以左旋字符串中的k个字符。
+//例如：
+//ABCD左旋一个字符得到BCDA
+//ABCD左旋两个字符
+
+void left_handed(char *p,int size,int count)
+{
+	int left = 0;
+	int right = size - 1;
+	while (count)
+	{
+		char tmp = p[0];
+		int i = 0;
+		for (i = 1; i <= size - 1; i++)
+		{
+			p[i - 1] = p[i];
+		}
+		p[size - 1] = tmp;
+		count--;
+	}
+	int j = 0;
+	for (j = 0; j < size; j++)
+	{
+		printf("%c", p[j]);
+	}
+	printf("\n");
+}
 
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int e = 0;
-	for (a = 1; a <= 5; a++)
+	int size;
+	int count;
+	int j = 0;
+	printf("请输入打算左旋的个数：  ");
+	scanf("%d", &count);
+	char arr[] = "ABCDEF";
+	size = sizeof(arr) / sizeof(arr[3]) - 1;
+	for (j = 0; j < size; j++)
 	{
-		for (b = 1; b <= 5; b++)
-		{
-			for (c = 1; c <= 5; c++)
-			{
-				for (d = 1; d <= 5; d++)
-				{
-					for (e = 1; e <= 5; e++)
-					{
-						if (((b == 2) + (a == 3) == 1) && ((b == 2) + (e == 4) == 1) && ((c == 1) + (d == 2) == 1) && ((c == 5) + (d == 3) == 1) && ((e == 4) + (a == 1) == 1) == 1)
-						{
-							if ((a != b) && (a != c) && (a != d) && (a != e) && (b != c) && (b != d) && (b != e) && (c != d) && (c != e) && (d != e))
-							{
-								printf("A=第%d名，B=第%d名，C=第%d名，D=第%d名，E=第%d名\n", a, b, c, d, e);
-							}
-						}
-					}
-				}
-			}
-		}
+		printf("%c", arr[j]);
 	}
+	printf("\n");
+	left_handed(arr,size,count);
 	return 0;
 }
