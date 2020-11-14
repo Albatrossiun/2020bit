fdd5870907f031e315c717af3676ab74a7e8d3e7
Albatrossiun <459627902@qq.com>
2020-11-14 17:04:58 +0800

    initially

--- TestCode/main.c
+++ TestCode/main.c
@@ -2,30 +2,57 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    int a, b, c;
-    while ((scanf("%d %d %d", &a, &b, &c) != EOF))
-    {
-        getchar();
-        if ((a + b > c) && (a + c > b) && (b + c > a))
-        {
-            if (a == b && a == c)
-            {
-                printf("Equilateral triangle!\n");
-            }
-            else if (a == b || b == c || a == c) 
-            {
-                printf("Isosceles triangle!\n");
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
 
-            }
-            else
+int main() 
+{
+    int n, i, j, flag = 1;
+    scanf("%d", &n);
+    int a[30] = { 0 };
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+        for (j = 0; j < i; j++) 
+        {
+            if (a[i] == a[j]) 
             {
-                printf("Ordinary triangle!\n");
+                flag = 0;
+                break;
             }
         }
-        else {
-            printf("Not a triangle!\n");
-        }
+        if (flag)
+            printf("%d ", a[i]);
+        flag = 1;
     }
     return 0;
 }
+
+//int main() 
+//{
+//    int a, b, c;
+//    while ((scanf("%d %d %d", &a, &b, &c) != EOF))
+//    {
+//        if ((a + b > c) && (a + c > b) && (b + c > a))
+//        {
+//            if (a == b && a == c)
+//            {
+//                printf("Equilateral triangle!\n");
+//            }
+//            else if (a == b || b == c || a == c) 
+//            {
+//                printf("Isosceles triangle!\n");
+//            }
+//            else
+//            {
+//                printf("Ordinary triangle!\n");
+//            }
+//        }
+//        else 
+//        {
+//            printf("Not a triangle!\n");
+//        }
+//    }
+//    return 0;
+//}
