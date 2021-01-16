44b074b27f370ee487978d73544b68ca6a62c190
Albatrossiun <459627902@qq.com>
2021-01-16 22:27:29 +0800

    BinaryTree

---
+++ test_2021_1_15_btree/test.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef char BTDataType;
+
+typedef struct BinaryTreeNode
+{
+	BTDataType _data;
+	struct BinaryTreeNode* _left;
+	struct BinaryTreeNode* _right;
+}BTNode;
+
+// 通过前序遍历的数组"ABD##E#H##CF##G##"构建二叉树
+BTNode* BinaryTreeCreate(BTDataType* src, int n, int* idx)
+{
+	if (src[*idx] == '#')
+	{
+		(*idx)++;
+		return NULL;
+	}
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	node->_data = src[*idx];
+	(*idx)++;
+
+	node->_left = BinaryTreeCreate(src, n, idx);
+	node->_right = BinaryTreeCreate(src, n, idx);
+	return node;
+}
+// 二叉树销毁
+void BinaryTreeDestory(BTNode** root)
+{
+	if (*root)
+	{
+		BinaryTreeDestory(&(*root)->_left);
+		BinaryTreeDestory(&(*root)->_right);
+		free(*root);
+		*root = NULL;
+	}
+}
+
+// 二叉树节点个数
+int BinaryTreeSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	return BinaryTreeSize(root->_left) + BinaryTreeSize(root->_right) + 1;
+}
+
+// 二叉树叶子节点个数
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	if (root->_left == NULL && root->_right == NULL)
+		return 1;
+	return BinaryTreeLeafSize(root->_left) + BinaryTreeLeafSize(root->_right);
+}
+
+// 二叉树第k层节点个数
+int BinaryTreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+		return 0;
+	if (k == 1)
+		return 1;
+	return BinaryTreeLevelKSize(root->_left, k - 1) + BinaryTreeLevelKSize(root->_right, k - 1);
+}
+
+// 二叉树查找值为x的节点
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x)
+{
+	if (root)
+	{
+		if (root->_data == x)
+			return root;
+		BTNode* node = BinaryTreeFind(root->_left, x);
+		if (node)
+			return node;
+		else
+			return BinaryTreeFind(root->_right, x);
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+// 二叉树前序遍历 
+void BinaryTreePrevOrder(BTNode* root)
+{
+	if (root)
+	{
+		printf("%c ", root->_data);
+		BinaryTreePrevOrder(root->_left);
+		BinaryTreePrevOrder(root->_right);
+	}
+}
+
+// 二叉树中序遍历
+void BinaryTreeInOrder(BTNode* root)
+{
+	if (root)
+	{
+		BinaryTreeInOrder(root->_left);
+		printf("%c ", root->_data);
+		BinaryTreeInOrder(root->_right);
+	}
+}
+
+// 二叉树后序遍历
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root)
+	{
+		BinaryTreePostOrder(root->_left);
+		BinaryTreePostOrder(root->_right);
+		printf("%c ", root->_data);
+	}
+}
+
+// 层序遍历
+void BinaryTreeLevelOrder(BTNode* root);
+
+// 判断二叉树是否是完全二叉树
+int BinaryTreeComplete(BTNode* root);
+
+// 二叉树深度
+int BinaryTreeHeight(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	return BinaryTreeHeight(root->_left) > BinaryTreeHeight(root->_right) ? BinaryTreeHeight(root->_left) + 1 : BinaryTreeHeight(root->_right) + 1;
+}
+
+void test()
+{
+	char src[] = "ABD##E#H##CF##G##";
+	int idx = 0;
+	BTNode* node = BinaryTreeCreate(src, 0, &idx);
+	BinaryTreePrevOrder(node);
+	printf("\n");
+	BinaryTreeInOrder(node);
+	printf("\n");
+	BinaryTreePostOrder(node);
+	printf("\n");
+}
+
+int main()
+{
+	test();
+	return 0;
+}
