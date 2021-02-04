08a938a1734ab79b8d29fb01facf172d20ea7a32
Albatrossiun <459627902@qq.com>
2021-02-04 23:44:32 +0800

    class(2)_Date

--- test_2021_2_2_class(1)/class.cpp
+++ test_2021_2_2_class(1)/class.cpp
@@ -3,54 +3,156 @@
 #include <iostream>
 using namespace std;
 
-int* ptr = NULL;
-class SeqList
-{
-public:
-	SeqList()
-	{
-		_size = 0;
-		p = (int*)malloc(sizeof(int));
-		cout << "SeqList()" << endl;
-	}
-	~SeqList()
-	{
-		free(p);
-		p = NULL;
-		_size = 0;
-		cout << "~SeqList()" << endl;
-	}
-	int _size;
-	int* p;
-};
 
-class Data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 运算符重载
+class Date
 {
 public:
-	Data(int day, int month)
+	Date(int month = 1, int day = 1)
 	{
-		_day = day;
 		_month = month;
-		cout << "Data(int day, int month)" << endl;
+		_day = day;
+		cout << "Date(int month = 1, int day = 1)" << endl;
 	}
 
-	Data()
+	bool operator==(const Date& d2)
 	{
-		cout << "Data()" << endl;
+		return _month == d2._month && _day == d2._day;
+		cout << "bool operator==(const Date& d2)" << endl;
+	}
+
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)
+		{
+			_month = d._month;
+			_day = d._day;
+			cout << "void operator=(const Date& d)" << endl;
+		}
+		return *this;
+		//return d;
 	}
 private:
-	int _day;
 	int _month;
-
-	SeqList sl;
+	int _day;
 };
 
 int main()
 {
-	Data d;
+	Date d1(1, 1);
+	Date d2(2, 2);
+	Date d3(3, 3);
+	//这里需要注意的是，左操作数是this指向的调用函数的对象
+	//if (d1 == d2)
+	
+	//d1 = d2;
+	d1.operator=(d2.operator=(d3));
+
+	/*
+	if (d1.operator==(d2))
+	{
+		cout << "==" << endl;
+	}
+	else
+	{
+		cout << "!=" << endl;
+	}
+	*/
+
 	return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+//int* ptr = NULL;
+//class SeqList
+//{
+//public:
+//	SeqList(int a)
+//	{
+//		_size = 0;
+//		p = (int*)malloc(sizeof(int));
+//		cout << "SeqList()" << endl;
+//	}
+//	~SeqList()
+//	{
+//		free(p);
+//		p = NULL;
+//		_size = 0;
+//		cout << "~SeqList()" << endl;
+//	}
+//	int _size;
+//	int* p;
+//};
+//
+//class Data
+//{
+//public:
+//	// 无参的构造函数
+//	Data()
+//	{
+//		cout << "Data()" << endl;
+//	}
+//	Data(int day, int month)
+//	// 有参的构造函数
+//	{
+//		_day = day;
+//		_month = month;
+//		cout << "Data(int day, int month)" << endl;
+//	}
+//	// 拷贝构造函数
+//	Data(const Data& d)
+//	{
+//		_day = d._day;
+//		_month = d._month;
+//	}
+//private:
+//	int _day;
+//	int _month;
+//};
+//
+//int main()
+//{
+//	//SeqList sl1(0);
+//	//SeqList sl2(sl1);
+//	//return 0;
+//	Data d1;
+//	Data d2(d1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	Data d;
+//	return 0;
+//}
 //void test()
 //{
 //	SeqList sl;
